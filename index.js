c64e2150407744bdcfbd85a66501284122fa2a0e
beberfabricio <beberfabricio@gmail.com>
2022-12-14 09:50:02 -0300

    modificacion de cors

--- index.js
+++ index.js
@@ -4,22 +4,10 @@ const mongoose = require('mongoose');
 const routes = require('./src/routes');
 const cors = require('cors')
 
-
 const app = express();
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  });
-  
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use(cors());
-
 
 
 app.get('/', (req,res) => {
