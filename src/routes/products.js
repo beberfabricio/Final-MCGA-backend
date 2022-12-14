d4299b0ddb80bdba0516ed7ba54012c93ee49704
beberfabricio <beberfabricio@gmail.com>
2022-12-13 23:09:17 -0300

    se agregan nuevamente los middlewares y se acomoda el vercel

--- src/routes/products.js
+++ src/routes/products.js
@@ -4,12 +4,12 @@ const authMiddleware = require('../middlewares/auth');
 
 router.get('/', getProducts)
 
-router.get('/search/:name', searchProduct)
+router.get('/search/:name', authMiddleware, searchProduct)
 
-router.post("/add", addProduct)
+router.post("/add", authMiddleware, addProduct)
 
-router.delete("/delete/:id", deleteProduct)
+router.delete("/delete/:id", authMiddleware, deleteProduct)
 
-router.put("/update/:id", updateProduct)
+router.put("/update/:id", authMiddleware, updateProduct)
 
 module.exports = router;
