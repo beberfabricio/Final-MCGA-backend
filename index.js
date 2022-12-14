b0900688793c545607ee08d140001f0b27b76dd4
beberfabricio <beberfabricio@gmail.com>
2022-12-13 22:55:39 -0300

    agregado de cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./src/routes');
+const cors = require('cors')
 
 const app = express();
 app.use(express.json());
 app.use(routes);
+app.use(cors());
 
 app.get('/', (req,res) => {
     res.send("OK");
